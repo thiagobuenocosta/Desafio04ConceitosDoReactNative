ce7e55ac9355dd6fae4ed057961a2545649ddcfb
Thiago Bueno Costa <th.bueno.95@gmail.com>
2020-12-10 17:05:24 -0300

    feat: connection with API of repositories in the end points of like and list the repositories

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3333",
+  baseURL: "http://192.168.15.48:3333",
+  // baseURL: "http://localhost:3333",
 });
+
+export const listRepositories = () => {
+  return api.get('repositories')
+}
+
+export const likeRepositorie = (id) => {
+  return api.post(`repositories/${id}/like`)
+}
 
 export default api;
